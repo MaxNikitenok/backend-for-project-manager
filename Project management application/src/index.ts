b4905d354edda052d87ea98def9f2f40359b4fc4
Max_Nikitenok <xxx250790@gmail.com>
2023-01-27 22:28:30 +0300

    feat: add user & password

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -6,7 +6,7 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect('mongodb+srv://jsdilitant:1fBhavPcWDszM4Lt@cluster0.doro2ku.mongodb.net/?retryWrites=true&w=majority');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
